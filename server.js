f6ef6dff9f78d5b56043a0b59e89672707b3a24c
Alex Grigorovich <alex.grigorovich@gmail.com>
2014-12-26 11:59:31 +0600

    In-memory countr server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var count = 0;
+app.get('/likes', function(req, res) {
+  res.json({count: count});
+});
+
+app.post('/likes', function(req, res) {
+  count += 1;
+  res.json({count: count});
+});
+
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Countr server listening at http://%s:%s', host, port);
+});
